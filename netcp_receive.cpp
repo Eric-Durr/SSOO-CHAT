6830aee1379f18cf8ed51bf960a9c6ca0e116d20
Eric Durr <alu0101027005@ull.edu.es>
2019-12-07 15:58:58 +0000

    making socket and message structure

--- netcp_receive.cpp
+++ netcp_receive.cpp
@@ -7,8 +7,37 @@
 #include<sys/fcntl.h>
 #include "socket.h"
 
+sockaddr_in make_ip_address(const std::string& ip_address, int port);
+
+
 int main (void){
+    Message msg_to_receive;
+    sockaddr_in senderIp = make_ip_address ("0.0.0.1", 1 );
+    sockaddr_in receiverIp = make_ip_address ("0.0.0.2", 2 );
+    Socket receiverSocket(receiverIp);
     
+    receiverSocket.receive_from(msg_to_receive, senderIp);
 
     return 0;
+}
+
+sockaddr_in make_ip_address(const std::string& ip_address, int port)
+{
+
+  sockaddr_in local_address{};
+
+  local_address.sin_family = AF_INET;  
+  
+  // Dependiendo de la dirección dada se 
+  if (ip_address == "") {
+    local_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  } else {
+    // opción igualmente válida (al parecer)
+    // local_address.sin_addr.s_addr = inet_addr(ip_address.c_str());
+    inet_aton(ip_address.c_str(), &(local_address.sin_addr));
+    
+  }
+  
+  local_address.sin_port = htons(port);  
+  return local_address;
 }
--- netcp_send.cpp
+++ netcp_send.cpp
@@ -8,16 +8,38 @@
 #include<sys/fcntl.h>
 #include "socket.h"
 
-sockaddr_in make_ip_address(const std::string& ip_adrress, int prot);
+sockaddr_in make_ip_address(const std::string& ip_address, int port);
 
 int main (void){
 
-
-    Socket senderSocket();
+    Message msg_to_send;
+    sockaddr_in senderIp = make_ip_address ("0.0.0.1", 1 );
+    sockaddr_in receiverIp = make_ip_address ("0.0.0.2", 2 );
+    
+    Socket senderSocket(senderIp);
+    senderSocket.send_to(msg_to_send, receiverIp);
     return 0;
 }
 
 
-sockaddr_in make_ip_address(const std::string& ip_adrress, int prot) {
 
+sockaddr_in make_ip_address(const std::string& ip_address, int port)
+{
+
+  sockaddr_in local_address{};
+
+  local_address.sin_family = AF_INET;  
+  
+  // Dependiendo de la dirección dada se 
+  if (ip_address == "") {
+    local_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  } else {
+    // opción igualmente válida (al parecer)
+    // local_address.sin_addr.s_addr = inet_addr(ip_address.c_str());
+    inet_aton(ip_address.c_str(), &(local_address.sin_addr));
+    
+  }
+  
+  local_address.sin_port = htons(port);  
+  return local_address;
 }
