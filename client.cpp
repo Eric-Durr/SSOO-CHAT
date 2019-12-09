45597ec04191f42237aabc1f687e38232db17831
Eric Durr <alu0101027005@ull.edu.es>
2019-12-09 18:47:11 +0000

    now working in one side

--- client.cpp
+++ client.cpp
@@ -6,49 +6,66 @@
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
+
+// Constants for conection:
+
 #define SERVER "127.0.0.1"
-#define BUFLEN 512
+#define BUFLEN 2048
 #define PORT 8888
 
 
 int main()
 {
-    struct sockaddr_in si_other;
-    int s, i, slen = sizeof(si_other);
-    char buf[BUFLEN];
-    char message[BUFLEN];
+    // local variables that will define our sockets:
+    
+    struct sockaddr_in si_other;       // --> Socket adress that will define the serverside
+    int s, i, slen = sizeof(si_other); // --> Integer values and dimentions that we will use
+    char buf[BUFLEN];                  // --> buffer size
+    char message[BUFLEN];              // --> message that will be sended through the socket
 
+    // Creation and checking of the socket (if the result isn't higer than 0, the program is ended)    
     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
             return 1;
     }
 
+    // Memory setting for socket address
     memset((char*) &si_other, 0, sizeof(si_other));
 
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(PORT);
+    // establishing adress parameters:
 
-    if ( inet_aton(SERVER, &si_other.sin_addr) == 0)
+    si_other.sin_family = AF_INET;      // -->  Socket kind of family
+    si_other.sin_port = htons(PORT);    // -->  Socket port (must be the same as the server)
+
+    if ( inet_aton(SERVER, &si_other.sin_addr) == 0) // --> Socket IP address stablishing and convertion (if the result isn't 0 the program is ended up)
     {
         return 2;
     }
 
+    // Concetion loop (eternal)
     while(1) { 
-        printf("Enter message: ");
-        gets(message);
 
+        // Asking for the message that we will send
+        printf("Enter message: ");
+        std::string aux;
+        std::cin >> aux;
+        aux.copy(message,sizeof(message));
+
+        // Sending the message to the socket established
         socklen_t temp = slen;
         if ((sendto(s, message, strlen(message), 0, (struct sockaddr*) &si_other, temp)) == -1) {
             
             return 3;
         }
+        // buffer stablishing of memory 
 
         memset(buf, '\0', BUFLEN);
 
-        if ((recvfrom(s, buf, BUFLEN, 0,
-             (struct sockaddr*) &si_other,(socklen_t*) &slen)) == -1 ) {
-            return 4;
-        }
-        puts(buf);
+        // receiving the feedback message
+        //if ((recvfrom(s, buf, BUFLEN, 0,
+        //     (struct sockaddr*) &si_other,(socklen_t*) &slen)) == -1 ) {
+        //    return 4;
+        //}
+        //puts(buf);
     }
 
     close(s);
--- server.cpp
+++ server.cpp
@@ -6,6 +6,9 @@
 #include<arpa/inet.h>
 #include<sys/socket.h>
 
+
+// Port and buffer constant definition
+
 #define BUFLEN 512
 #define PORT 8888
 
@@ -17,6 +20,7 @@ void die(char *s)
 
 int main()
 {
+  // Sockets creation (server and client) [adresses, size variables, definition, and adress initialization]   
     struct sockaddr_in si_me, si_other;
 
     int s, i, slen = sizeof(si_other) , recv_len;
@@ -31,28 +35,36 @@ int main()
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(PORT);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-
+  
     if(bind(s, (struct sockaddr*) &si_me, sizeof(si_me)) == -1) {
             return 2;
         
     }
+  //
+    
+    // calling loop
 
     while(1){
+    
         std::cout << "waiting for data ... \n";
         fflush(stdout);
+
+        // receive call
 
         if ((recv_len = recvfrom(s, buf, BUFLEN, 0,
                                  (struct sockaddr*) &si_other,(socklen_t*) &slen)) == -1 ) {
             return 3;
         }
-
+        // printing info
         printf("Received package from %s:%d \n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
         printf("Data: %s\n", buf);
-        socklen_t temp = slen;
-        if ((sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, temp)) == -1) {
-            
-            return 4;
-        }
+
+        // send call
+        //socklen_t temp = slen;
+        //if ((sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, temp)) == -1) {
+        //    
+        //    return 4;
+        //}
     }
 
     close(s);
