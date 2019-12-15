aca4f2149ad9c4c33ed6f066c0757324d53e4226
Eric Durr <alu0101027005@ull.edu.es>
2019-12-15 20:03:33 +0000

    renaming

---
+++ netcp_client_.cpp
@@ -0,0 +1,96 @@
+// This program should open "prueba.txt", read it and send it to the receiver socket
+// librerías:
+
+#include<iostream>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/fcntl.h>
+#include "socket.cpp"
+
+// Constantes:
+
+#define BUFLEN  1024
+#define PORT    8888    
+
+// Prototipos:
+
+sockaddr_in make_ip_address(const std::string& ip_address, int port);
+
+int main (int argc, char *argv[]){
+
+  if ( argc == 2) {
+    // variables:
+      struct sockaddr_in  server_address, client_address;
+      int                 addrlen = sizeof(server_address), recv_len, ret;
+      char                buffer[BUFLEN];
+
+    //
+
+    // File oppening:
+      int fileFd = open(argv[1], O_RDONLY);
+      if (fileFd < 0) {
+          std::cerr << "Falló la apertura del fichero: " << '\n';
+          return  1;
+      }
+    //
+
+    // Address: 
+      memset((char *) &server_address, 0, sizeof(server_address));
+      server_address = make_ip_address("", PORT);
+    //
+
+    // Socket:
+      Socket<Message> server_socket(server_address);
+
+    //
+
+    while(1){
+      struct Message message;
+
+      std::cout << "Sending file ... \n";
+      
+      server_socket.receive_from(message, client_address);
+      
+      std::cout << "conected to [" << inet_ntoa(client_address.sin_addr)
+      << ":" << ntohs(client_address.sin_port) << "]" <<std::endl;
+      
+      while((ret = read(fileFd, message.text.data(), message.text.size()-1)) > 0) {
+
+        message.text.data()[ret] = 0x00;
+        server_socket.send_to(message, client_address); 
+      }
+
+    }
+    if ( int is_closed = close(fileFd) < 0) {
+  
+          std::cerr << "Falló el cierre del fichero: " << '\n';
+          return  2;    
+    }
+  } else {
+    std::cout << "falta el nombre del fichero a enviar " << std::endl;
+  
+  }
+}
+
+
+sockaddr_in make_ip_address(const std::string& ip_address, int port)
+{
+  sockaddr_in address;
+
+  address.sin_family = AF_INET;  
+  
+  address.sin_port = htons(port);  
+  // Dependiendo de la dirección dada se 
+  if (ip_address == "") {
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+  } else {
+
+    // opción igualmente válida (al parecer)
+    // local_address.sin_addr.s_addr = inet_addr(ip_address.c_str());
+    inet_aton(ip_address.c_str(), &(address.sin_addr));
+    
+  }
+
+  return address;
+}
--- netcp_server.cpp
+++ netcp_server.cpp
@@ -1,7 +1,9 @@
-// This program should open "prueba.txt", read it and send it to the receiver socket
-// librerías:
+// This program should open socket which will eait to receive the file info and
+// show it's content through the screen.
 
 #include<iostream>
+#include<stdio.h>
+
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<sys/fcntl.h>
@@ -9,6 +11,7 @@
 
 // Constantes:
 
+#define SERVER "127.0.0.1"
 #define BUFLEN  1024
 #define PORT    8888    
 
@@ -16,81 +19,59 @@
 
 sockaddr_in make_ip_address(const std::string& ip_address, int port);
 
-int main (int argc, char *argv[]){
+// Main program:
 
-  if ( argc == 2) {
-    // variables:
-      struct sockaddr_in  server_address, client_address;
-      int                 addrlen = sizeof(server_address), recv_len, ret;
-      char                buffer[BUFLEN];
+int main (void){
+  // variables:
+    struct sockaddr_in  client_address;
+    int                 addrlen = sizeof(client_address);
+    char                buffer[BUFLEN];
+    struct Message      message;
 
-    //
-
-    // File oppening:
-      int fileFd = open(argv[1], O_RDONLY);
-      if (fileFd < 0) {
-          std::cerr << "Falló la apertura del fichero: " << '\n';
-          return  1;
-      }
-    //
-
-    // Address: 
-      memset((char *) &server_address, 0, sizeof(server_address));
-      server_address = make_ip_address("", PORT);
-    //
-
-    // Socket:
-      Socket<Message> server_socket(server_address);
-
-    //
-
-    while(1){
-      struct Message message;
-
-      std::cout << "Sending file ... \n";
-      
-      server_socket.receive_from(message, client_address);
-      
-      std::cout << "conected to [" << inet_ntoa(client_address.sin_addr)
-      << ":" << ntohs(client_address.sin_port) << "]" <<std::endl;
-      
-      while((ret = read(fileFd, message.text.data(), message.text.size()-1)) > 0) {
-
-        message.text.data()[ret] = 0x00;
-        server_socket.send_to(message, client_address); 
-      }
-
+    
+  // dirección:
+    memset((char*) &client_address, 0, sizeof(client_address));
+    client_address = make_ip_address(SERVER, PORT);
+    if (client_address.sin_addr.s_addr == 0) {
+      printf("Error en la creación de la dirección del socket");
+      return 1;
     }
-    if ( int is_closed = close(fileFd) < 0) {
-  
-          std::cerr << "Falló el cierre del fichero: " << '\n';
-          return  2;    
+
+  // socket:
+    Socket<Message> client_socket(client_address);
+    message.text.at(0) = '.';
+  // try-catch loop
+    while(1 && message.text.at(0) == '.') {
+      std::cout << "Lectura del fichero en el servidor: \n";
+      client_socket.send_to(message, client_address);
+      client_socket.receive_from(message, client_address);
+      std::cout << "Datos recibidos: \n\n\n";
+      std::cout << message.text.data();
+      std::cout << "\n\n\n";
     }
-  } else {
-    std::cout << "falta el nombre del fichero a enviar " << std::endl;
-  
-  }
+
+    return 0;
 }
 
 
+// Functions
+
 sockaddr_in make_ip_address(const std::string& ip_address, int port)
 {
-  sockaddr_in address;
+  sockaddr_in local_address;
 
-  address.sin_family = AF_INET;  
+  local_address.sin_family = AF_INET;  
+  local_address.sin_port = htons(port);  
   
-  address.sin_port = htons(port);  
   // Dependiendo de la dirección dada se 
   if (ip_address == "") {
-    address.sin_addr.s_addr = htonl(INADDR_ANY);
-    
+    local_address.sin_addr.s_addr = htonl(INADDR_ANY);
   } else {
-
-    // opción igualmente válida (al parecer)
+   
     // local_address.sin_addr.s_addr = inet_addr(ip_address.c_str());
-    inet_aton(ip_address.c_str(), &(address.sin_addr));
+    inet_aton(ip_address.c_str(), &(local_address.sin_addr));
     
   }
-
-  return address;
+  
+  return local_address;
 }
